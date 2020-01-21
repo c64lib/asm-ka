ef303c88f01e1f764c518d96c79e5ccd66a4ef45
Maciej Malecki <malecki.maciej@gmail.com>
2020-01-21 22:56:55 +0100

    revert to version

--- check.sh
+++ check.sh
@@ -6,7 +6,7 @@ rm KickAssembler.zip
 
 wget --quiet http://theweb.dk/KickAssembler/_FrontPage.html
 
-VERSION=V`cat _FrontPage.html | grep -Po '(?<=Kick Assembler V)\d+.\d.'`
+VERSION=`cat _FrontPage.html | grep -Po '(?<=Kick Assembler V)\d+.\d.'`
 echo "Found version ${VERSION}"
 
 HTTP_CODE=`curl -s -o /dev/null -I -w "%{http_code}" https://github.com/c64lib/asm-ka/releases/tag/${VERSION}`
