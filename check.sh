784c45df7da185653031b95bbe48fec23904e120
Maciej Malecki <malecki.maciej@gmail.com>
2020-01-21 23:06:28 +0100

    final check

--- check.sh
+++ check.sh
@@ -6,17 +6,20 @@ rm KickAssembler.zip
 
 wget --quiet http://theweb.dk/KickAssembler/_FrontPage.html
 
-VERSION=`cat _FrontPage.html | grep -Po '(?<=Kick Assembler V)\d+.\d.'`
+VERSION=V`cat _FrontPage.html | grep -Po '(?<=Kick Assembler V)\d+.\d.'`
 echo "Found version ${VERSION}"
 
 HTTP_CODE=`curl -s -o /dev/null -I -w "%{http_code}" https://github.com/c64lib/asm-ka/releases/tag/${VERSION}`
 echo "Version ${VERSION} released?: ${HTTP_CODE}"
 
-wget --quiet http://theweb.dk/KickAssembler/KickAssembler.zip
-unzip KickAssembler.zip
-
-if [ $HTTP_CODE != '200' ]
+if [ $HTTP_CODE == '404' ]
 then
 	git tag $VERSION
 	git push origin $VERSION
+fi
+
+if [ $TRAVIS_TAG != '' ]
+then
+	wget --quiet http://theweb.dk/KickAssembler/KickAssembler.zip
+	unzip KickAssembler.zip
 fi
