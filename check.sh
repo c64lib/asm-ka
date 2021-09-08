b98512c21dbf9dfeee5017d80fd3376d7b7da36a
Maciej Malecki <maciej.malecki@capgemini.com>
2021-09-08 07:29:46 +0200

    add fail

--- check.sh
+++ check.sh
@@ -6,6 +6,13 @@ rm version
 wget -d --user-agent="Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0" http://theweb.dk/KickAssembler/_FrontPage.html
 
 VERSION=`cat _FrontPage.html | grep -Po '(?<=Kick Assembler V)\d+.\d.'`
+
+if [ "$VERSION" == "" ]
+then
+	echo "KickAssembler version cannot be determined, maybe the webpage cannot be downloaded or its format has been changed."
+	exit -1
+fi
+
 echo "Found version ${VERSION}"
 
 HTTP_CODE=`curl -s -o /dev/null -I -w "%{http_code}" https://github.com/c64lib/asm-ka/releases/tag/${VERSION}`
