a84e1fb58516abc7f3834c695c0bdebccb765d41
Maciej Malecki <maciej.malecki@capgemini.com>
2021-09-08 07:21:37 +0200

    act as mozilla

--- download.sh
+++ download.sh
@@ -4,7 +4,7 @@
 if [ "$CIRCLE_TAG" != "" ]
 then
 	rm KickAssembler.zip
-	wget --quiet http://theweb.dk/KickAssembler/KickAssembler.zip
+	wget -d --user-agent="Mozilla/5.0 (Windows NT x.y; rv:10.0) Gecko/20100101 Firefox/10.0" http://theweb.dk/KickAssembler/KickAssembler.zip
 	unzip KickAssembler.zip
 	cp KickAss.jar out
 fi
